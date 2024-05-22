9ad6670d6dbbbca7bb4d15d0a3b1d068caf60d3a
marketakvasova <85996091+marketakvasova@users.noreply.github.com>
2024-05-22 13:21:02 +0200

    Add files via upload

---
+++ install_libraries.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Installing necessary Python libraries..."
+pip install numpy
+pip install opencv-python
+pip install torch
+pip install segmentation_models_pytorch
+pip install albumentations
+echo "Installation complete."
